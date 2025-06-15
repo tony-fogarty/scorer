105e1390dc93fb09dd2441cbbf95b23eaf335c56
Tony Fogarty <tony.fogarty@creativoso.uk>
2025-06-15 10:27:20 +0100

    tidy up

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\Http\Controllers\GameController;
-use App\Http\Controllers\PlayerController;
-use App\Http\Controllers\SettingsController;
 
 Route::get('/', fn() => Inertia::render('Welcome'))->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', fn() => Inertia::render('Dashboard'))->name('dashboard');
-    Route::get('/game-settings', [SettingsController::class, 'index'])->name('game-settings');
-    Route::get('/game/{id}', [GameController::class, 'show'])->name('game.show');
-    Route::post('/game', [GameController::class, 'store'])->name('game.store');
-    Route::patch('/game/{id}/state', [GameController::class, 'updateState'])->name('game.updateState');
-    Route::get('/game/{id}/summary', [GameController::class, 'showSummary'])->name('game.summary');
-    Route::get('/games', [GameController::class, 'index'])->name('games.index');
-    Route::resource('players', PlayerController::class);
-    Route::post('/game/{game}/save-stats', [GameController::class, 'saveStats']);
 });
 
 require __DIR__.'/settings.php';
